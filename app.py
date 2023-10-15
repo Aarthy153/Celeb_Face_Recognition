5ccab57151f2c2950d6a06ec37771ef50e43a9a6
Aarthy Ramesh <69035013+Aarthy153@users.noreply.github.com>
2023-10-15 04:58:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import cv2
+from PIL import Image
 import numpy as np
 import joblib
 
@@ -9,17 +9,17 @@ model = joblib.load('saved_model.pkl')
 # Define a function to process the uploaded image
 def process_image(uploaded_image):
     # Convert the image to grayscale (if needed)
-    uploaded_image = cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2GRAY)
+    uploaded_image = uploaded_image.convert('L')
 
     # Resize the image to match the input size expected by the model
     input_size = (64, 64)  # Adjust this to match the input size expected by your model
-    uploaded_image = cv2.resize(uploaded_image, input_size)
+    uploaded_image = uploaded_image.resize(input_size)
 
     # Flatten the image to match the input shape expected by the model
-    uploaded_image_flattened = uploaded_image.flatten().reshape(1, -1)
+    uploaded_image_array = np.array(uploaded_image).flatten().reshape(1, -1)
 
     # Predict using the trained model
-    predicted_class = model.predict(uploaded_image_flattened)
+    predicted_class = model.predict(uploaded_image_array)
 
     class_labels = {0: 'Serena Williams', 1: 'Lionel Messi', 2: 'Maria Sharapova'}
     predicted_celebrity = class_labels.get(predicted_class[0], 'Unknown')
@@ -33,7 +33,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     # Read the image
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+    image = Image.open(uploaded_file)
 
     # Display the uploaded image
     st.image(image, caption='Uploaded Image.', use_column_width=True)
